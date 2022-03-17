1f3badaf46de8514472f1612c92a0eb967edee56
davidmospina <cuartomauri@gmail.com>
2022-03-17 14:58:28 +0100

    Fixed: The unit degree conversor is full operative

--- tema4/conversor.py
+++ tema4/conversor.py
@@ -9,17 +9,17 @@ class Conversor:
         self.labelCantidad = tk.Label(self.ventana, text= "Cantidad:")
         self.labelCantidad.grid(row=0, column=0)
 
-        self.cantidad=tk.StringVar()
+        self.cantidad=tk.StringVar(value= "0.0")
         self.entryCantidad=tk.Entry(self.ventana, width=10, textvariable=self.cantidad)
         self.entryCantidad.grid(column=1, row=0)
 
         #Seleccion unidades iniciales
         self.initType=tk.IntVar()
-        self.radioKelvin=tk.Radiobutton(self.ventana,text="Kelvin", variable=self.initType, value=1)
+        self.radioKelvin=tk.Radiobutton(self.ventana,text="Kelvin", variable=self.initType, value=0)
         self.radioKelvin.grid(column=0, row=1)
-        self.radioCelcius=tk.Radiobutton(self.ventana,text="Celcius", variable=self.initType, value=2)
+        self.radioCelcius=tk.Radiobutton(self.ventana,text="Celcius", variable=self.initType, value=1)
         self.radioCelcius.grid(column=1, row=1)
-        self.radioFahrenheit=tk.Radiobutton(self.ventana,text="Fahrenheit", variable=self.initType, value=3)
+        self.radioFahrenheit=tk.Radiobutton(self.ventana,text="Fahrenheit", variable=self.initType, value=2)
         self.radioFahrenheit.grid(column=2, row=1)
 
         #Boton de conversion
@@ -51,10 +51,10 @@ class Conversor:
         if self.finalType[0].get() == True:
             self.kConversion=tk.Label(self.ventana, text = "Kelvin")
             self.kConversion.grid(column=0, row=nConversiones)
-            if self.initType.get() == 2:
-                self.valorK = float(self.cantidad) + 273.15
-            elif self.initType.get() == 3:
-                self.valorK = 5/9*(float(self.cantidad) - 32)+273.15
+            if self.initType.get() == 1:
+                self.valorK = float(self.cantidad.get()) + 273.15
+            elif self.initType.get() == 2:
+                self.valorK = 5/9*(float(self.cantidad.get()) - 32)+273.15
             
             self.finalKelin=tk.Label(self.ventana, text = self.valorK, foreground= "blue")
             self.finalKelin.grid(column=1, row=nConversiones)
@@ -63,10 +63,10 @@ class Conversor:
         if self.finalType[1].get() == True:
             self.cConversion=tk.Label(self.ventana, text = "Celcius")
             self.cConversion.grid(column=0, row=nConversiones)
-            if self.initType.get() == 1:
-                self.valorC = float(self.cantidad) - 273.15
-            elif self.initType.get() == 3:
-                self.valorC = (float(self.cantidad) -32)/1.8
+            if self.initType.get() == 0:
+                self.valorC = float(self.cantidad.get()) - 273.15
+            elif self.initType.get() == 2:
+                self.valorC = (float(self.cantidad.get()) -32)/1.8
             self.finalCelcius=tk.Label(self.ventana, text = self.valorC, foreground= "blue")
             self.finalCelcius.grid(column=1, row=nConversiones)
             nConversiones+=1
@@ -75,10 +75,13 @@ class Conversor:
 
             self.fConversion=tk.Label(self.ventana, text = "Fahrenheit")
             self.fConversion.grid(column=0, row=nConversiones)
+            
             if self.initType.get() == 1:
-                self.valorF = float(self.cantidad) * 1.8 + 32
-            elif self.initType.get() == 2:
-                self.valorF = 1.8*(float(self.cantidad) - 273.15) +32  
+                self.valorF = float(self.cantidad.get()) * 1.8 + 32
+                print(self.valorF)
+
+            elif self.initType.get() == 0:
+                self.valorF = 1.8*(float(self.cantidad.get()) - 273.15) +32  
             self.finalFahrenheit=tk.Label(self.ventana, text = self.valorF, foreground= "blue")
             self.finalFahrenheit.grid(column=1, row=nConversiones)
             nConversiones+=1        
@@ -86,5 +89,3 @@ class Conversor:
 
 Conversor1 = Conversor()
 Conversor1.ventana.mainloop()
-
-
