aa03cc19e61611284e212015a3d3662540fece80
davidmospina <cuartomauri@gmail.com>
2022-03-21 17:58:21 +0100

    .

--- tema4/conversor3.py
+++ tema4/conversor3.py
@@ -1,85 +1,89 @@
 import tkinter as tk
 from tkinter import ttk
-from ttkthemes import ThemedStyle
-
 
 class Conversor:
     def __init__(self):
             
         self.ventana = tk.Tk()
-        self.ventana.title("Conversor de Temperaturas")
-        style = ThemedStyle(self.ventana)
-        style.set_theme("radiance")
 
-        #Campor de cantidad a covertir
-        self.labelCantidad = ttk.Label(self.ventana, text= "Cantidad:")
+        self.labelframe1=ttk.LabelFrame(self.ventana, text="Datos de Entrada:")        
+        self.labelframe1.grid(column=0, row=0, padx=5, pady=10) 
+
+        #Campo de cantidad a covertir
+        self.labelCantidad = tk.Label(self.labelframe1, text= "Cantidad:")
         self.labelCantidad.grid(row=0, column=0)
 
         self.cantidad=tk.StringVar(value= "0.0")
-        self.entryCantidad=ttk.Entry(self.ventana, width=10, textvariable=self.cantidad)
+        self.entryCantidad=tk.Entry(self.labelframe1, width=10, textvariable=self.cantidad)
         self.entryCantidad.grid(column=1, row=0)
 
         #Seleccion unidades iniciales
         self.initType=tk.IntVar()
-        self.radioKelvin=ttk.Radiobutton(self.ventana,text="Kelvin", variable=self.initType, value=0)
+        self.radioKelvin=tk.Radiobutton(self.labelframe1,text="Kelvin", variable=self.initType, value=0)
         self.radioKelvin.grid(column=0, row=1)
-        self.radioCelcius=ttk.Radiobutton(self.ventana,text="Celcius", variable=self.initType, value=1)
+        self.radioCelcius=tk.Radiobutton(self.labelframe1,text="Celcius", variable=self.initType, value=1)
         self.radioCelcius.grid(column=1, row=1)
-        self.radioFahrenheit=ttk.Radiobutton(self.ventana,text="Fahrenheit", variable=self.initType, value=2)
+        self.radioFahrenheit=tk.Radiobutton(self.labelframe1,text="Fahrenheit", variable=self.initType, value=2)
         self.radioFahrenheit.grid(column=2, row=1)
+
+        self.labelframe2=ttk.LabelFrame(self.ventana, text="Datos Conversión:")        
+        self.labelframe2.grid(column=0, row=1, padx=5, pady=10)  
 
         #Boton de conversion
 
-        self.ConversionButton=ttk.Button(self.ventana, text="Convertir a:", command=self.calcularConversion)
+        self.ConversionButton=tk.Button(self.labelframe2, text="Convertir a:", command=self.calcularConversion)
         self.ConversionButton.grid(column=1, row=2)
-
-       
 
         #Seleccion unidades finales
         self.finalType = [tk.BooleanVar(False),tk.BooleanVar(False),tk.BooleanVar(False)]
-        self.check3=ttk.Checkbutton(self.ventana,text="Kelvin", variable=self.finalType[0], onvalue= True)
+        self.check3=tk.Checkbutton(self.labelframe2,text="Kelvin", variable=self.finalType[0], onvalue= True)
         self.check3.grid(column=0, row=3)
-        self.check3=ttk.Checkbutton(self.ventana,text="Celcius", variable=self.finalType[1], onvalue= True)
+        self.check3=tk.Checkbutton(self.labelframe2,text="Celcius", variable=self.finalType[1], onvalue= True)
         self.check3.grid(column=1, row=3)
-        self.check3=ttk.Checkbutton(self.ventana,text="Fahrenheit", variable=self.finalType[2], onvalue= True)
+        self.check3=tk.Checkbutton(self.labelframe2,text="Fahrenheit", variable=self.finalType[2], onvalue= True)
         self.check3.grid(column=2, row=3)
 
-
+        self.labelframe3=tk.LabelFrame(self.ventana, text="Resultados:")
 
 
     def calcularConversion(self):
-        
+
+        self.labelframe3.destroy()
+        self.labelframe3=tk.LabelFrame(self.ventana, text="Resultados:")  
+        self.labelframe3.grid(column=0, row=2, padx=5, pady=10, sticky ="WE")        
+ 
+
         self.valorC = 0
         self.valorF = 0
         self.valorK = 0
         nConversiones = 4
 
         if self.finalType[0].get() == True:
-            self.kConversion=ttk.Label(self.ventana, text = "Kelvin")
+            self.kConversion=tk.Label(self.labelframe3, text = "Kelvin")
             self.kConversion.grid(column=0, row=nConversiones)
             if self.initType.get() == 1:
                 self.valorK = float(self.cantidad.get()) + 273.15
             elif self.initType.get() == 2:
                 self.valorK = 5/9*(float(self.cantidad.get()) - 32)+273.15
             
-            self.finalKelin=ttk.Label(self.ventana, text = self.valorK, foreground= "blue")
+            self.finalKelin=tk.Label(self.labelframe3, text = self.valorK, foreground= "blue")
             self.finalKelin.grid(column=1, row=nConversiones)
             nConversiones+=1
         
         if self.finalType[1].get() == True:
-            self.cConversion=ttk.Label(self.ventana, text = "Celcius")
+            self.cConversion=tk.Label(self.labelframe3, text = "Celcius")
             self.cConversion.grid(column=0, row=nConversiones)
             if self.initType.get() == 0:
                 self.valorC = float(self.cantidad.get()) - 273.15
             elif self.initType.get() == 2:
                 self.valorC = (float(self.cantidad.get()) -32)/1.8
-            self.finalCelcius=ttk.Label(self.ventana, text = self.valorC, foreground= "blue")
+            self.finalCelcius=tk.Label(self.labelframe3, text = self.valorC, foreground= "blue")
             self.finalCelcius.grid(column=1, row=nConversiones)
             nConversiones+=1
 
         if self.finalType[2].get() == True:
 
-            self.fConversion=ttk.Label(self.ventana, text = "Fahrenheit")
+            self.fConversion=tk.Label(self.labelframe3, text = "Fahrenheit")
             self.fConversion.grid(column=0, row=nConversiones)
             
             if self.initType.get() == 1:
@@ -88,7 +92,7 @@ class Conversor:
 
             elif self.initType.get() == 0:
                 self.valorF = 1.8*(float(self.cantidad.get()) - 273.15) +32  
-            self.finalFahrenheit=ttk.Label(self.ventana, text = self.valorF, foreground= "blue")
+            self.finalFahrenheit=tk.Label(self.labelframe3, text = self.valorF, foreground= "blue")
             self.finalFahrenheit.grid(column=1, row=nConversiones)
             nConversiones+=1        
 
