84f4b1213f25c0fc7444d33b6537d1eb99edf9df
davidmospina <cuartomauri@gmail.com>
2022-03-25 17:28:01 +0100

    Tema6: Ejericios 2 y 5 funcionando bombastic

--- tema6/ejericicio5.py
+++ tema6/ejericicio5.py
@@ -4,15 +4,21 @@ import threading
 import time
 
 class player(threading.Thread):
+    __cerrojo = threading.Lock()
     def __init__ (self, pID):
         threading.Thread.__init__(self)
         self.numero = 0
         self.ID = pID
+        
 
     def run(self):
         while True:
             self.numero = random.randint(1,100)
-            print("P" + str(self.ID) + ": " + str(self.numero)+ "\n")
+        
+            player.__cerrojo.acquire()
+            print("P" + str(self.ID) + ": " + str(self.numero))
+            player.__cerrojo.release()
+
             allNumberChoseen.wait()
             endGame.wait()
 
@@ -26,11 +32,11 @@ class Director(threading.Thread):
             
     def elegirGanador(self):
         if self.p0.numero > self.p1.numero and self.p0.numero > self.p2.numero:
-            print("Gana el P" + str(self.p0.ID)+ "\n")
+            print("Gana el P" + str(self.p0.ID))
         elif self.p1.numero > self.p0.numero and self.p1.numero > self.p2.numero:
-            print("Gana el P" + str(self.p1.ID) + "\n")
+            print("Gana el P" + str(self.p1.ID))
         elif self.p2.numero > self.p0.numero and self.p2.numero > self.p1.numero:
-            print("Gana el P" + str(self.p2.ID) + "\n")
+            print("Gana el P" + str(self.p2.ID))
         
         print()
         
